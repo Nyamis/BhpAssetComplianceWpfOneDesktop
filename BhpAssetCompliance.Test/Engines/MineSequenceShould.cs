aea2549bc1fa009cce3cfa6e24bfee4f7447fa91
FranciscoSotoCavieres <74250485+FranciscoSotoCavieres@users.noreply.github.com>
2021-08-25 10:53:27 -0400

    Cambios generales

---
+++ BhpAssetCompliance.Test/Engines/MineSequenceShould.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using BhpAssetComplianceWpfOneDesktop.Engines;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeOpenXml;
+
+namespace BhpAssetCompliance.Test.Engines
+{
+    [TestClass]
+    public class MineSequenceShould
+    {
+        [TestInitialize]
+        public void Test()
+        {
+            // SOLID
+            // Single Purpose Class 
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        }
+
+        [TestMethod]
+        public void QuartersReconciliationFactorsLoadTemplateFixture()
+        {
+            var path = @"..\..\..\TestData\FilledTemplates\ReconciliationFactorsTemplate.xlsx";
+            var exists =File.Exists(path);
+
+            // var reconcilationEngine = new QuarterReconciliationFactorsLoadTemplateEngine();
+        }
+    }
+}
---
+++ BhpAssetComplianceWpfOneDesktop/Engines/QuarterReconciliationFactorsLoadTemplateEngine.cs
@@ -0,0 +1,17 @@
+﻿namespace BhpAssetComplianceWpfOneDesktop.Engines
+{
+    public class QuarterReconciliationFactorsLoadTemplateEngine
+    {
+        private readonly QuarterReconciliationFactors _factors;
+
+        public QuarterReconciliationFactorsLoadTemplateEngine(QuarterReconciliationFactors factors)
+        {
+            _factors = factors;
+        }
+
+        public void LoadTemplate()
+        {
+            
+        }
+    }
+}
