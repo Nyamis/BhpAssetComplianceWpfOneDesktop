f36bff1c6e91a3fc675302ceea9d9f2483e307d2
FranciscoSotoCavieres <galarcono@uft.edu>
2021-09-14 07:00:43 -0300

    140921

---
+++ BhpAssetCompliance.Test/Engines/MineSequenceShould.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using BhpAssetComplianceWpfOneDesktop.Engines;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeOpenXml;
+
+namespace BhpAssetCompliance.Test.Engines
+{
+    [TestClass]
+    public class MineSequenceShould
+    {
+        [TestInitialize]
+        public void Test()
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        }
+
+        [TestMethod]
+        public void QuartersReconciliationFactorsLoadTemplateFixture()
+        {
+            var path = @"..\..\..\TestData\FilledTemplates\ReconciliationFactorsTemplate.xlsx";
+            var exists =File.Exists(path);
+
+            var reconciliationEngine = new QuartersReconciliationFactorsReadTemplate(path);
+            
+        }
+    }
+}
